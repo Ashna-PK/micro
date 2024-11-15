8a7ea1d81b88d5810960fefdc8dead5d22b1ef65
Ashna P K <120293262+Ashna-PK@users.noreply.github.com>
2024-11-15 20:10:46 +0530

    4 microservices completed

---
+++ services/Seller.Api/Models/Shop.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Seller.Api.Models
+{
+    public class Shop
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }=string.Empty;
+        [Required]
+        public string Description { get; set; }=string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now; //auto         
+        public bool IsVerified { get; set; } = false; //admin   // Flag to show if shop is verified        
+        public string Email { get; set; } = string.Empty;       //verify email           
+        public string PhoneNumber { get; set; } = string.Empty;
+        [Required]
+        public bool isOpen { get; set; } //no need
+        public DateTime openTime { get; set; }
+        public DateTime closeTime { get; set; }
+        public decimal rating { get; set; }    //admin+user
+    }
+}
---
+++ services/Seller.Api/Repository/IRepository/IShopRepository.cs
@@ -0,0 +1,15 @@
+﻿using Seller.Api.Data;
+using Seller.Api.Models;
+
+namespace Seller.Api.Repository.IRepository
+{
+    public interface IShopRepository
+    {
+        public Task<IEnumerable<Shop>> getShops();
+        public Task<Shop> addShopDetails( shopDto shop);
+        public Task<bool> deleteShop(int id);
+        public Task<Shop> editShop(int id, shopDto shop);
+        public Task<Shop> getShopById(int Id);
+        public Task<bool> adminVerify(int id, bool isVerified, decimal rating);
+    }
+}
